f360e5a01f39267971399599680e6b9c1d4074ac
guboc11 <guboc11@gmail.com>
2024-02-14 17:34:29 +0900

    Add a basic api

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func mint(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("mint success"))
+
+}
 
 func main() {
 	fmt.Println("hello Planetarium")
+
+	http.HandleFunc("/mint", mint)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
