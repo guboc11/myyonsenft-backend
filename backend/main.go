e79e17d85d8120b8e8127b44a900c2165ded11a6
guboc11 <guboc11@gmail.com>
2024-02-14 21:25:55 +0900

    Remove unused type Payload

--- backend/main.go
+++ backend/main.go
@@ -6,13 +6,6 @@ import (
 	"net/http"
 )
 
-type Payload struct {
-	Jsonrpc string `json:"jsonrpc"`
-	ID      int64  `json:"id"`
-	Method  string `json:"method"`
-	Params  []any  `json:"params"`
-}
-
 func mint(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("mint success"))
 
