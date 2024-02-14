cc2543ecd6918befc2debdb60cc2de6a25faed69
guboc11 <guboc11@gmail.com>
2024-02-14 17:42:34 +0900

    Add balanceOf function

--- backend/main.go
+++ backend/main.go
@@ -11,10 +11,15 @@ func mint(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func balanceOf(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("get balance of"))
+}
+
 func main() {
 	fmt.Println("hello Planetarium")
 
 	http.HandleFunc("/mint", mint)
+	http.HandleFunc("/balanceOf", balanceOf)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
