422dea7e9ac7078988ec456af5bada6d86d41d24
guboc11 <guboc11@gmail.com>
2024-02-14 21:10:32 +0900

    Update simpleContractCall() to viewBalanceOf()

--- backend/main.go
+++ backend/main.go
@@ -6,10 +6,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/big"
 	"net/http"
 	"strings"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -58,16 +61,58 @@ func simpleAPICall() {
 	log.Println("result", result)
 }
 
-func simpleContractCall() {
+func viewBalanceOf() {
 	client, err := ethclient.Dial("https://rpc.holesky.ethpandaops.io")
 	if err != nil {
 		log.Fatal(err)
 	}
+	// fmt.Println("connect success")
 
-	msg := ethereum.CallMsg{}
-	client.CallContract(context.Background(), msg, nil)
-	fmt.Println("called msg")
+	from := common.HexToAddress("0xF8c847Fc824B441f0b4D9641371e6eD3f56CF145")
+	to := common.HexToAddress("0xd16d41635c7ece3c13b2c7eae094a92adf41bb2a")
 
+	// 호출할 함수와 인자 데이터 ABI 인코딩
+	const definition = `
+	[{"inputs":[{"internalType":"address","name":"account","type":"address"}],
+	"name":"balanceOf",
+	"outputs":[{"internalType":"uint256","name":"","type":"uint256"}],
+	"stateMutability":"view",
+	"type":"function"}]
+	`
+
+	abi, err := abi.JSON(strings.NewReader(definition))
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := abi.Pack("balanceOf", from)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("data : %x\n", data)
+
+	msg := ethereum.CallMsg{
+		From:  from,
+		To:    &to,
+		Gas:   200_000_000,
+		Data:  data,
+		Value: big.NewInt(0),
+	}
+
+	resp, err := client.CallContract(context.Background(), msg, nil)
+	if err != nil {
+		log.Fatalf("Call Contract error : %v", err)
+	}
+	// fmt.Println("response :", resp)
+
+	// uint256 반환값 추출
+	returnValue, err := abi.Unpack("balanceOf", resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("반환된 값:", returnValue)
+
+	fmt.Println("called balanceOf")
 }
 
 func mint(w http.ResponseWriter, r *http.Request) {
@@ -76,12 +121,12 @@ func mint(w http.ResponseWriter, r *http.Request) {
 }
 
 func getBalanceOf(w http.ResponseWriter, r *http.Request) {
-	// simpleAPICall()
-	simpleContractCall()
+	viewBalanceOf()
 	w.Write([]byte("get balance of"))
 }
 
 func main() {
+
 	http.HandleFunc("/mint", mint)
 	http.HandleFunc("/balanceOf", getBalanceOf)
 
