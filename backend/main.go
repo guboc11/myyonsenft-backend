b428911c8a9812bd9f2ed395448a08b5003ea3ab
guboc11 <guboc11@gmail.com>
2024-02-14 21:14:12 +0900

    Remove simpleAPICall() function and unused print line codes

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"math/big"
 	"net/http"
@@ -23,53 +21,12 @@ type Payload struct {
 	Params  []any  `json:"params"`
 }
 
-func simpleAPICall() {
-	data, err := json.Marshal(Payload{
-		Jsonrpc: "2.0",
-		ID:      1,
-		Method:  "eth_call",
-		Params: []interface{}{
-			map[string]interface{}{
-				"from": nil,
-				"data": "0x70a08231000000000000000000000000f8c847fc824b441f0b4d9641371e6ed3f56cf145",
-				"to":   "0xd16d41635c7ece3c13b2c7eae094a92adf41bb2a",
-			},
-			"latest",
-		},
-	})
-	if err != nil {
-		log.Fatalf("Marshal: %v", err)
-	}
-	resp, err := http.Post(
-		"https://rpc.holesky.ethpandaops.io",
-		"application/json",
-		strings.NewReader(string(data)),
-	)
-	if err != nil {
-		log.Fatalf("Post: %v", err)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("ReadAll: %v", err)
-	}
-	var result interface{}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-	}
-	log.Println("result", result)
-}
-
 func viewBalanceOf() {
+	// ethereum client 생성
 	client, err := ethclient.Dial("https://rpc.holesky.ethpandaops.io")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("connect success")
-
-	from := common.HexToAddress("0xF8c847Fc824B441f0b4D9641371e6eD3f56CF145")
-	to := common.HexToAddress("0xd16d41635c7ece3c13b2c7eae094a92adf41bb2a")
 
 	// 호출할 함수와 인자 데이터 ABI 인코딩
 	const definition = `
@@ -84,11 +41,14 @@ func viewBalanceOf() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	from := common.HexToAddress("0xF8c847Fc824B441f0b4D9641371e6eD3f56CF145")
+	to := common.HexToAddress("0xd16d41635c7ece3c13b2c7eae094a92adf41bb2a")
+
 	data, err := abi.Pack("balanceOf", from)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Printf("data : %x\n", data)
 
 	msg := ethereum.CallMsg{
 		From:  from,
@@ -102,7 +62,6 @@ func viewBalanceOf() {
 	if err != nil {
 		log.Fatalf("Call Contract error : %v", err)
 	}
-	// fmt.Println("response :", resp)
 
 	// uint256 반환값 추출
 	returnValue, err := abi.Unpack("balanceOf", resp)
@@ -110,9 +69,7 @@ func viewBalanceOf() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("반환된 값:", returnValue)
-
-	fmt.Println("called balanceOf")
+	fmt.Println("result", returnValue)
 }
 
 func mint(w http.ResponseWriter, r *http.Request) {
