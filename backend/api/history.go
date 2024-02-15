03c27a02a443eb824afb0a497f9f7712b19994cd
guboc11 <guboc11@gmail.com>
2024-02-15 13:51:59 +0900

    Update GetTxHistory() function logic

--- backend/api/history.go
+++ backend/api/history.go
@@ -1,7 +1,43 @@
 package api
 
-import "github.com/ethereum/go-ethereum/ethclient"
+import (
+	"fmt"
+	"sort"
 
-func GetHistory(client *ethclient.Client, address string) {
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func GetTxHistory(client *ethclient.Client, address string) {
+	sort.Slice(txHistory[address], func(i, j int) bool {
+		// 먼저 CreatedAt 내림차순 정렬
+		if txHistory[address][i].CreatedAt.After(txHistory[address][j].CreatedAt) {
+			return true
+		} else if txHistory[address][i].CreatedAt.Before(txHistory[address][j].CreatedAt) {
+			return false
+		}
+
+		// CreatedAt이 같을 경우 Tx 이 "" 일 떄 순서가 먼저
+		if txHistory[address][i].Tx == "" && txHistory[address][j].Tx != "" {
+			return true
+		} else if txHistory[address][i].Tx != "" && txHistory[address][j].Tx == "" {
+			return false
+		}
+
+		// 둘 다 Tx != "" 일 경우 SUCCESS 순서가 나중
+		if txHistory[address][i].Status == "SUCCESS" {
+			return false
+		} else {
+			return true
+
+		}
+	})
+
+	// 100개 까지 출력
+	for i, txStatus := range txHistory[address] {
+		if i >= 100 {
+			break
+		}
+		fmt.Println(i, txStatus)
+	}
 
 }
--- backend/main.go
+++ backend/main.go
@@ -75,7 +75,7 @@ func main() {
 	// /history endpoint
 	go http.HandleFunc("/history", func(w http.ResponseWriter, r *http.Request) {
 		address := r.URL.Query().Get("address")
-		api.GetHistory(client, address)
+		api.GetTxHistory(client, address)
 
 		// JSON으로 변환하여 응답
 		w.Header().Set("Content-Type", "application/json")
